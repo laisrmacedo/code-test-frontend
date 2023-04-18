067399587c05d13988ec78e352efd78a33bd072e
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-04-18 19:53:20 -0300

    feat: POST request

--- codeleap-network/src/components/CreatePost.js
+++ codeleap-network/src/components/CreatePost.js
@@ -1,4 +1,8 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
+import { BASE_URL } from '../utils/constants'
 
 const Container = styled.div`
   display: flex;
@@ -13,12 +17,12 @@ const Container = styled.div`
     color: #000;
   }
 
-  div{
+  form{
+    /* border: 1px red solid; */
     width:100%;
     display: flex;
     flex-direction: column;
-    /* border: 1px red solid; */
-  }
+    gap: 16px;
 
   p{
     font-size: 16px;
@@ -41,27 +45,77 @@ const Container = styled.div`
     align-self: flex-end;
     width: 111px;
     height: 32px;
-    background: #7695EC;
     border-radius: 8px;
     border: none;
     color: #FFF;
+    background: ${(props) => ((props.title.length === 0 || props.content.length === 0)? `#cfdbff`: `#7695EC`)};
+    cursor: ${(props) => ((props.title.length === 0 || props.content.length === 0)? `auto`: `pointer`)};
   }
+}
 `
 
-
 export const CreatePost = () => {
+  const [form, setForm] = useState({
+    title: "",
+    content: ""
+  })
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    createPost()
+  }
+
+  const onChangeForm = (e) => {
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
+
+  const {currentUser} = useSelector((rootReducer) => rootReducer.reducer)
+
+  const body = {
+    username: currentUser,
+    title: form.title,
+    content: form.content
+  }
+
+  const createPost = async () => {
+    try {
+      await axios.post(BASE_URL, body)
+      setForm({
+        title: "",
+        content: ""
+      })
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+
   return (
-    <Container>
+    <Container title={form.title} content={form.content}>
       <h2>What’s on your mind?</h2>
-      <div>
-        <p>Title</p>
-        <input placeholder='Hello world' />
-      </div>
-      <div>
-        <p>Content</p>
-        <textarea placeholder='Content here' />
-      </div>
-      <button>Create</button>
+      <form onSubmit={handleClick}>
+        <div>
+          <p>Title</p>
+          <input placeholder='Hello world'
+            required
+            type="text"
+            name="title"
+            value={form.title}
+            onChange={onChangeForm}
+          />
+        </div>
+        <div>
+          <p>Content</p>
+          <textarea placeholder='Content here' 
+            required
+            type="text"
+            name="content"
+            value={form.content}
+            onChange={onChangeForm}
+          />
+        </div>
+        <button disabled={(form.title.length === 0 || form.content.length === 0)? true : false}>Create</button>
+      </form>
     </Container>
   )
 }
