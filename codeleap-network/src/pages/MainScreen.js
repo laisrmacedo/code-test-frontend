5432c7056f28c35c011b853010a1f26eda5a742c
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-04-18 19:54:09 -0300

    feat: posts page

--- codeleap-network/src/pages/MainScreen.js
+++ codeleap-network/src/pages/MainScreen.js
@@ -2,7 +2,15 @@ import styled from 'styled-components'
 import { Body } from '../components/styledComponents'
 import { CreatePost } from '../components/CreatePost'
 import { Post } from '../components/Post'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from "axios";
+import { BASE_URL } from '../utils/constants'
+import { useNavigate } from 'react-router-dom'
+import { goToSignupPage } from '../router/coordinator'
+import { useDispatch } from 'react-redux'
+import { logoutUser } from '../actions/actions'
+import { useSelector } from "react-redux";
+
 
 const Container = styled.div`
   display: flex;
@@ -16,20 +24,33 @@ const Container = styled.div`
     width: 100%;
     min-height: 80px;
     background: #7695EC;
-    padding-left: 37px;
+    padding: 0 37px;
     display: flex;
     align-items: center;
+    justify-content: space-between;
 
     p{
       font-size: 22px;
       font-weight: 700;
       color: #FFFFFF;
     }
+
+    button{
+      width: 111px;
+      height: 32px;
+      background: transparent;
+      border-radius: 8px;
+      border: 1px solid #FFF;
+      color: #FFF;
+      cursor: pointer;
+      &:hover{
+        background-color: #8facfe;
+        transition: background-color 0.3s ease-in-out;
+      }
+    }
   }
 
   main{
-    border: 1px red solid;
-    overflow-y: auto;
     width: 100%;
     min-height: calc(100% - 80px);
     padding: 24px;
@@ -37,40 +58,59 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     gap: 24px;
-    /* position: relative; */
-
+    
     .createPost{
       display: flex;
       flex-direction: column;
       width: 100%;
-      /* position: sticky;
-      top: 0; */
-      /* height: 350px; */
       min-height: 350px;
-    /* align-items: center; */
     }
-
+    
     .posts{
-      >div{
-      height: 400px;
-
-      }
+      display: flex;
+      flex-direction: column;
+      gap: 24px;
+      min-height: 1500px;
+      width: 100%;
     }
   }
+  
+  `
 
+const PostContainer = styled.div`
+  min-height: ${(props) => (props.height < 100 ? `181px` : 180+Math.floor(props.height/100)*19 + `px`)};
 `
 
 export const MainScreen = () => {
-
+  const navigate = useNavigate()
   const [posts, setPosts] = useState([])
 
-  const getPosts = async (path, headers) => {
+  const getPosts = async () => {
     try {
-      // const responde = await axios.get(BASE_URL + path, headers)
-      // setPosts(responde.data)
+      const responde = await axios.get(BASE_URL)
+      setPosts(responde.data.results)
     } catch (error) {
       console.log(error.response.data)
     }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [posts])
+
+  const {currentUser} = useSelector((rootReducer) => rootReducer.reducer)
+
+  useEffect(() => {
+    if(currentUser === null){
+      goToSignupPage(navigate)
+    }
+  }, [])
+  
+  const dispatch = useDispatch()
+
+  const logout = () => {
+    dispatch(logoutUser())
+    goToSignupPage(navigate)
   }
 
   return (
@@ -78,16 +118,20 @@ export const MainScreen = () => {
       <Container>
         <header>
           <p>CodeLeap Network</p>
+          <button onClick={() => logout()}>Logout</button>
         </header>
         <main>
           <div className='createPost'>
             <CreatePost></CreatePost>
           </div>
           <div className='posts'>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
+          {posts?.map((post)=>{
+            return(
+              <PostContainer key={post.id} height={post.content?.length}>
+                <Post data={post}/>
+              </PostContainer>
+            )
+          })}
           </div>
         </main>
       </Container>
