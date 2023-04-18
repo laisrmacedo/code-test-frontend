f69e23b1c6d81f4d0c3a1b2554132d7296889089
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-04-18 20:32:02 -0300

    feat: deletePost function request

--- codeleap-network/src/components/Post.js
+++ codeleap-network/src/components/Post.js
@@ -3,6 +3,8 @@ import deleteImage from '../assets/delete.png'
 import penImage from '../assets/edit-pen.png'
 import boxImage from '../assets/edit-box.png'
 import { useSelector } from 'react-redux'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants'
 
 const Container = styled.div`
   display: flex;
@@ -63,13 +65,21 @@ const Container = styled.div`
 export const Post = (props) => {
   const {currentUser} = useSelector((rootReducer) => rootReducer.reducer)
 
+  const deletePost = async (id) => {
+    try {
+      await axios.delete(BASE_URL + id + '/')
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+
   return (
-    <Container showDiv={props.data.username === currentUser? true : false}>
+    <Container showDiv={props.post.username === currentUser? true : false}>
       <div className='title'>
-        <h2>{props.data.title}</h2>
+        <h2>{props.post.title}</h2>
         <div>
-          <img src={deleteImage}/>
-          <span>
+          <img src={deleteImage} onClick={() => deletePost(props.post.id)}/>
+          <span onClick={() => editPost(props.post.id)}>
             <img className='pen' src={penImage}/>
             <img src={boxImage}/>
           </span>
@@ -77,10 +87,10 @@ export const Post = (props) => {
       </div>
       <div className='postData'>
         <span>
-          <h5>@{props.data.username}</h5>
-          <p>{props.data.created_datetime}</p>
+          <h5>@{props.post.username}</h5>
+          <p>{props.post.created_datetime}</p>
         </span>
-        <p>{props.data.content}</p>
+        <p>{props.post.content}</p>
       </div>
     </Container>
   )
