3f8f4356d1183e4d480f062f3e4bd75f4ff383f0
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-04-18 02:29:49 -0300

    feat: router to MainScreen

--- codeleap-network/src/router/Router.js
+++ codeleap-network/src/router/Router.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Signup } from "../pages/Signup";
+import { MainScreen } from "../pages/MainScreen";
 
 export const Router = () => {
   return(
     <BrowserRouter>
       <Routes>
         <Route index element={<Signup/>}/>
-        {/* <Route path={``} element={}/> */}
+        <Route path={`/posts`} element={<MainScreen/>}/>
       </Routes>
     </BrowserRouter>
   )
