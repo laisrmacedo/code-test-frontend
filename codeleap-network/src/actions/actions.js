3c677985511ec5a20516892f2beae2934b5b3d44
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-04-19 01:33:54 -0300

    feat: actions to modal states

--- codeleap-network/src/actions/actions.js
+++ codeleap-network/src/actions/actions.js
@@ -3,6 +3,20 @@ export const signupUser = (payload) => ({
   payload
 })
 
-export const logoutUser = (payload) => ({
+export const logoutUser = () => ({
   type: 'logout'
+})
+
+export const openModal = () => ({
+  type: 'modal-is-open'
+})
+
+export const closeModal = () => ({
+  type: 'modal-is-close'
+})
+
+export const getRequestData = (modal, id) => ({
+  type: 'request-data',
+  id,
+  modal
 })
