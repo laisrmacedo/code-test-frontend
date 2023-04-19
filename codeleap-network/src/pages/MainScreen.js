535b84d19e6a8156c416e28a2efc40a5ae57690c
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-04-19 13:54:46 -0300

    feat: back to top button and intinity scroll

--- codeleap-network/src/pages/MainScreen.js
+++ codeleap-network/src/pages/MainScreen.js
@@ -23,6 +23,10 @@ const Container = styled.div`
   width: 80%;
   max-width: 800px;
   height: 100%;
+  position: relative;
+  @media screen and (max-width: 425px){
+    width: 100%;
+  }
 
   header{
     width: 100%;
@@ -62,7 +66,29 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     gap: 24px;
+    position: relative;
+
+    #topBtn{
+      height: 28px;
+      font-variant: all-small-caps; 
+      background: #FFF;
+      border: none;
+      box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+      position: absolute;
+      top: 30px;
+      z-index: 10;
+      cursor: pointer;
+      display: ${(props) => (props.display)};
+    }
     
+    .posts{
+      display: flex;
+      flex-direction: column;
+      gap: 22px;
+      /* min-height: 1500px; */
+      width: 100%;
+    }
+
     .createPost{
       display: flex;
       flex-direction: column;
@@ -70,33 +96,42 @@ const Container = styled.div`
       min-height: 350px;
     }
     
-    .posts{
-      display: flex;
-      flex-direction: column;
-      gap: 24px;
-      min-height: 1500px;
-      width: 100%;
-    }
   }
   
   `
 
-const PostContainer = styled.div`
-  min-height: ${(props) => (props.height < 100 ? `181px` : 180+Math.floor(props.height/100)*19 + `px`)};
+const PostContainer = styled.li`
+  min-height: ${(props) => (props.height < 100 ? `181px` : 180+Math.floor(props.height/100)*22 + `px`)};
 `
 
 export const MainScreen = () => {
   const navigate = useNavigate()
   const [posts, setPosts] = useState([])
+  const [currentOffset, setCurrentOffset] = useState(5)
 
   const getPosts = async () => {
     try {
-      const responde = await axios.get(BASE_URL)
-      setPosts(responde.data.results)
+      const response = await axios.get(BASE_URL + `?limit=${currentOffset}`)
+      if(response.data.results.length === response.data.count){
+        setCurrentOffset(response.data.count)
+      }else{
+        setPosts(response.data.results)
+      }
     } catch (error) {
       console.log(error.response.data)
     }
   }
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if(entries.some((entry) => entry.isIntersecting)){
+        console.log('esta visivel')
+        setCurrentOffset((currentOffsetInsideState) => currentOffsetInsideState + 5)
+      }
+    })
+    intersectionObserver.observe(document.querySelector('#underObservation'))
+    return () => intersectionObserver.disconnect()
+  }, [])
 
   useEffect(() => {
     getPosts()
@@ -116,20 +151,39 @@ export const MainScreen = () => {
     dispatch(logoutUser())
     goToSignupPage(navigate)
   }
-  
+
+  const [displayBtn, setDisplayBtn] = useState("none")
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if(!entries.some((entry) => entry.isIntersecting)){
+        setDisplayBtn('block')
+      }else{
+        setDisplayBtn('none')
+      }
+    })
+    intersectionObserver.observe(document.querySelector('.createPost'))
+    return () => intersectionObserver.disconnect()
+  }, [])
+
+  const scrollToTop = () => {
+    window.document.querySelector('li').scrollIntoView({behavior: "smooth"})
+  }
+
 
   return (
     <Body>
-      <Container>
+      <Container display={displayBtn}>
         <header>
           <p>CodeLeap Network</p>
           <button onClick={() => logout()}>Logout</button>
         </header>
-        <main>
-          <div className='createPost'>
-            <NewPost component={'toCreate'}/>
-          </div>
-          <div className='posts'>
+        <main id='mainTag'>
+          <button id='topBtn' onClick={() => scrollToTop()}>back to top</button>
+          <ul className='posts'>
+            <li className='createPost'>
+              <NewPost component={'toCreate'}/>
+            </li>
           {posts?.map((post)=>{
             return(
               <PostContainer key={post.id} height={post.content?.length}>
@@ -137,7 +191,8 @@ export const MainScreen = () => {
               </PostContainer>
             )
           })}
-          </div>
+            <li id='underObservation'></li>
+          </ul>
         </main>
       </Container>
       <ReactModal
