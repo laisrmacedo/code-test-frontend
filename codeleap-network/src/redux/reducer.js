ebc3d81e575016cd7ce602f8bb5064c3a18a070d
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-04-19 01:34:40 -0300

    feat: cases to modal states

--- codeleap-network/src/redux/reducer.js
+++ codeleap-network/src/redux/reducer.js
@@ -1,16 +1,24 @@
-const initialUser = {
-  currentUser: null
+const initialState = {
+  currentUser: null,
+  isModalOpen: false,
+  clckedPostId: null,
+  modalRequest: null
 }
 
-export const reducer = (state = initialUser, action) => {
+export const reducer = (state = initialState, action) => {
 
   switch (action.type) {
     case 'signup':
       return { ...state, currentUser: action.payload}
     case 'logout':
       return { ...state, currentUser: null}
+    case 'modal-is-open':
+      return { ...state, isModalOpen: true}
+    case 'modal-is-close':
+      return { ...state, isModalOpen: false}
+    case 'request-data':
+      return { ...state, clckedPostId: action.id, modalRequest: action.modal}
     default:
       return state
   }
-
 }
